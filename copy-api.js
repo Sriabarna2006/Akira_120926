c395ea0b6c80751079eabc5d4fe6e30754721778
sriabarna A <shree200618@gmail.com>
2026-09-23 14:17:16 +0530

    completing processs

--- copy-api.js
+++ copy-api.js
@@ -7,4 +7,12 @@ const dest = path.resolve('api/[...all].js');
 if (fs.existsSync(src)) {
   fs.copyFileSync(src, dest);
   console.log('✓ Successfully created api/[...all].js catch-all serverless function');
+
+  const clientApiDir = path.resolve('client/api');
+  if (!fs.existsSync(clientApiDir)) {
+    fs.mkdirSync(clientApiDir, { recursive: true });
+  }
+  fs.copyFileSync(src, path.join(clientApiDir, 'index.js'));
+  fs.copyFileSync(src, path.join(clientApiDir, '[...all].js'));
+  console.log('✓ Successfully synced serverless functions to client/api/');
 }
